e428d4764901428c16f4134d3c4eeaaaa8da6d3c
Sigeon Oh <dhtlrjs5@naver.com>
2025-10-24 15:53:04 +0900

    test: Car 클래스 단위 테스트 추가

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,70 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    @DisplayName("자동차 객체 생성 테스트")
+    void newCarTest() {
+        //given
+        Car car = new Car("car");
+
+        //when
+
+        //then
+        assertThat(car.getName()).isEqualTo("car");
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차 이동 가능 테스트")
+    void carMoveReturnTrueTest() {
+        //given
+        Car car = new Car("car");
+
+        //when
+        car.move(() -> true);
+
+        //then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동차 이동 불가능 테스트")
+    void carMoveReturnFalseTest() {
+        //given
+        Car car = new Car("car");
+
+        //when
+        car.move(() -> false);
+
+        //then
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5글자를 초과하면 IllegalArgumentException 발생")
+    void carNameExceedMaxLengthTest() {
+        //given
+        String carName = "superCar";
+
+        //when, then
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 공백이면 IllegalArgumentException 발생")
+    void carNameIsBlankTest() {
+        //given
+        String carName = "";
+
+        //when, then
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
