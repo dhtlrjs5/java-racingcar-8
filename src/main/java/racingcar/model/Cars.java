0df96b37c950b545cee9967c8b108c31d3c1dc09
Sigeon Oh <dhtlrjs5@naver.com>
2025-10-24 18:54:58 +0900

    feat: 중복 이름 검증 기능 추가

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 public class Cars {
 
+    private static final String DUPLICATE_NAME_ERROR_MESSAGE = "중복된 자동차 이름이 있습니다.";
+
     private final List<Car> cars;
 
     private Cars(List<Car> cars) {
@@ -14,6 +16,8 @@ public class Cars {
     }
 
     public static Cars createCars(List<String> carNames) {
+        validateDuplicateName(carNames);
+
         return new Cars(carNames.stream()
                 .map(Car::new)
                 .collect(Collectors.toList()));
@@ -40,5 +44,15 @@ public class Cars {
                 .mapToInt(Car::getPosition)
                 .max()
                 .orElse(0);
+    }
+
+    private static void validateDuplicateName(List<String> carNames) {
+        long distinctSize = carNames.stream()
+                .distinct()
+                .count();
+
+        if (distinctSize != carNames.size()) {
+            throw new IllegalArgumentException(DUPLICATE_NAME_ERROR_MESSAGE);
+        }
     }
 }
