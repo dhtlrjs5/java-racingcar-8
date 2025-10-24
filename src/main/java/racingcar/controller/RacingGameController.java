a20834a8e440d15541ae779f9ec4def92e0287e8
Sigeon Oh <dhtlrjs5@naver.com>
2025-10-24 19:05:04 +0900

    feat: 입력 횟수 음수 검증 기능 추가

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -7,6 +7,8 @@ import racingcar.view.OutputView;
 
 public class RacingGameController {
 
+    private static final String ATTEMPT_COUNT_NEGATIVE_ERROR_MESSAGE = "시도 횟수는 0이상 이여야 합니다.";
+
     private final InputView inputView;
     private final OutputView outputView;
     private final MoveStrategy moveStrategy;
@@ -21,6 +23,10 @@ public class RacingGameController {
         Cars cars = Cars.createCars(inputView.inputCarNames());
         int attempts = inputView.inputAttemptCount();
 
+        if (isNegativeNumber(attempts)) {
+            throw new IllegalArgumentException(ATTEMPT_COUNT_NEGATIVE_ERROR_MESSAGE);
+        }
+
         outputView.outputResult();
 
         for (int i = 0; i < attempts; i++) {
@@ -29,5 +35,9 @@ public class RacingGameController {
         }
 
         outputView.outputFinalWinner(cars.getWinner());
+    }
+
+    private boolean isNegativeNumber(int number) {
+        return number < 0;
     }
 }
